7af8078d88583ee837ee5b45a0884ce83d86e7c5
akorinsichris <104876344+akorinsichris@users.noreply.github.com>
2023-12-19 15:57:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,16 @@
-hello world
+import streamlit as st
+import streamlit_gsheets import GSheetsConnection
+import pandas as pd
+
+# Display Title and Desription
+st.title("My Knowledge Base Portal")
+st.markdown("Enter the details of the new knowledge information below.")
+
+# Establishing a Google Sheets Connection
+conn = st.experimental_connection('gsheets', type=GSheetsConnection)
+
+#Fetch exist data
+existing_data = conn.read(worksheet="knowledgebase", usecols=list(range(4)), ttl=5)
+existing_data = existing_data.dropna(how="all")
+
+st.dataframe(existing_data)
