e960f4903f79124cfdd05c7fde56778c34ca58cc
akorinsichris <104876344+akorinsichris@users.noreply.github.com>
2023-12-19 16:20:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,29 @@ conn = st.experimental_connection('gsheets', type=GSheetsConnection)
 existing_data = conn.read(worksheet="knowledgebase", usecols=list(range(4)), ttl=5)
 existing_data = existing_data.dropna(how="all")
 
-st.dataframe(existing_data)
+# st.dataframe(existing_data)
+
+CATEGORY = [
+  "Linux",
+  "Technology",
+  "Snowflake",
+  "PostgreSQL",
+  "Teradata",
+]
+
+# onboarding a new kb form
+with st.form(key="kb_form"):
+  category_name = st.selectbox("Category*", options=CATEGORY)
+  subject = st.text_input(label="Subject*")
+  description = st.text_area(label="Description")
+
+# Mark mandatory fields
+st.markdown("**required*")
+
+submit_button = st.form_submit_button(lable="Submit")
+
+# if submit button is pressed
+if submit_button:
+  st.write("submit was pressed")
+  
+  
