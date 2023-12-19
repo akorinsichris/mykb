21a201fb6fa93338fb5f46663f769cabecde9fcf
akorinsichris <104876344+akorinsichris@users.noreply.github.com>
2023-12-19 16:38:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.markdown("Enter the details of the new knowledge information below.")
 conn = st.experimental_connection('gsheets', type=GSheetsConnection)
 
 #Fetch exist data
-existing_data = conn.read(worksheet="knowledgebase", usecols=list(range(4)), ttl=5)
+existing_data = conn.read(worksheet="knowledgebase", usecols=list(range(3)), ttl=5)
 existing_data = existing_data.dropna(how="all")
 
 # st.dataframe(existing_data)
@@ -28,6 +28,7 @@ with st.form(key="kb_form"):
   category_name = st.selectbox("Category*", options=CATEGORY)
   subject = st.text_input(label="Subject*")
   description = st.text_area(label="Description")
+  
 
 # Mark mandatory fields
 st.markdown("**required*")
@@ -36,6 +37,31 @@ submit_button = st.form_submit_button(lable="Submit")
 
 # if submit button is pressed
 if submit_button:
-  st.write("submit was pressed")
+  # check all mandatory field are filled
+  if not category_name or not subject:
+    st.warning("Ensure all mandatory fields are filled.")
+    st.stop()
+  elif existing_data["Subject"].str.contains(subject).any():
+    st.warning("Subject already exists.")
+    st.stop()
+  else:
+    # create a new row of kb data
+    kb_data = pd.DataFrame(
+      [
+        {
+          "Category": category_name,
+          "Subject": subject,
+          "Description": description,
+        }
+      ]
+    )
+    
+    # Add the new kb data to the existing data
+    updated_df = pd.concat([existing_data, kb_data], ignore_index=True)
+
+    # Update Google Sheets with the new kb data
+  conn.update(worksheet="knowledgebase", data=updated_df)
+  
+  st.write("Records Save")
   
   
